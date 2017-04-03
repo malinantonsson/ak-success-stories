ad4e55b083d5d75fb518bfa3bf008e38e6d7a7fa
Malin Antonsson <malin@malinantonsson.com>
2017-04-03 22:26:19 +0100

    adding date, content, title etc & trying out some more options

--- ak-success-stories.php
+++ ak-success-stories.php
@@ -16,12 +16,43 @@ function create_posttype() {
 			),
 			'public' => true,
 			'has_archive' => true,
+			'supports' => array('custom-fields'),
 			'rewrite' => array('slug' => 'success-stories'),
 		)
 	);
 }
 // Hooking up our function to theme setup
 add_action( 'init', 'create_posttype' );
+
+// Create the Team shortcode
+add_shortcode("success-story-archive", "successStoriesArchive_sc");
+
+function successStoriesArchive_sc($atts) {
+    global $post;
+
+    $args = array(
+    	'post_type' => 'success-stories', 
+    	'order'=> 'ASC', 
+    	'orderby' => 'date');
+
+    $custom_posts = get_posts($args);
+    ?>
+    <h2>Success story Archive</h2>
+    <?php
+
+    foreach($custom_posts as $post) : setup_postdata($post);
+        $output = 'im the output archive';
+        ?>
+        
+        <h3>date: <?php echo get_the_date('jS F Y'); ?> </h3>
+
+        <a href="<?php the_permalink(); ?>">title: <?php the_title(); ?></a>
+        
+	<?php
+    endforeach; wp_reset_postdata();
+	return $output;
+}
+
 
 // Create the Team shortcode
 add_shortcode("success-story-latest", "successStories_sc");
@@ -33,18 +64,27 @@ function successStories_sc($atts) {
     	'post_type' => 'success-stories', 
     	'posts_per_page' => 1, 
     	'order'=> 'ASC', 
-    	'orderby' => 'title');
+    	'orderby' => 'date');
 
     $custom_posts = get_posts($args);
 
     foreach($custom_posts as $post) : setup_postdata($post);
         $output = 'im the output';
         ?>
-        <a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
-        
-	<?php
-    endforeach; wp_reset_postdata();
-	return $output;
-}
+        <h2>Success story post</h2>
+        <a href="<?php the_permalink(); ?>">Link</a><br>
+        title: <?php the_title(); ?> <br>
+        content: <?php the_content(); ?>
+
+        <?php 
+	       $next_post = get_next_post();
+if ( is_a( $next_post , 'WP_Post' ) ) : ?>
+    <a href="<?php echo get_permalink( $next_post->ID ); ?>">Next post: <?php echo get_the_title( $next_post->ID ); ?></a>
+<?php endif; ?>
+ 
+ 	<?php 
+    	endforeach; wp_reset_postdata();
+		return $output;
+	}
 
 ?>
