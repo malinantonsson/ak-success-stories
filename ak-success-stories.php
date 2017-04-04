c06af8c53915eb6a8398bf9a4068692c16237bad
Malin Antonsson <malin@malinantonsson.com>
2017-04-04 21:51:06 +0100

    add plugin script

--- ak-success-stories.php
+++ ak-success-stories.php
@@ -24,6 +24,14 @@ function create_posttype() {
 add_action( 'init', 'create_posttype' );
 
 
+
+// Enqueue styles and scripts
+add_action( 'wp_enqueue_scripts', 'add_success_script' );
+function add_success_script() { 
+	wp_enqueue_script( 'ak-success-stories', plugins_url('ak-success-stories/js/success-script.js'), array ( 'jquery' ), 1.1, true); 
+}
+
+
 // Create the archive shortcodes
 add_shortcode("success-story-archive", "successStoriesArchive_sc");
 
@@ -38,20 +46,18 @@ function successStoriesArchive_sc($atts) {
 
     $custom_posts = get_posts($args);
     ?>
-    <h2>Success story Archive</h2>
-    <?php
-
-    foreach($custom_posts as $post) : setup_postdata($post);
-        $output = 'im the output archive';
-        ?>
-        
-        <h3>date: <?php echo get_the_date('jS F Y'); ?> </h3>
-
-        <a href="<?php the_permalink(); ?>">title: <?php the_title(); ?></a>
-        
+	    <nav class="success-archive" data-behaviour="success-stories">
+	    	<ul class="success-archive__list">
+		    <?php foreach($custom_posts as $post) : setup_postdata($post); ?>
+		    	<li class="success-archive__item">
+		    		<a class="success-archive__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		    		<span class="success-archive__date"><?php echo get_the_date('jS F Y'); ?></span>
+		    	</li>
+		        
+			<?php endforeach; wp_reset_postdata(); ?>
+			</ul>
+		</nav>
 	<?php
-    endforeach; wp_reset_postdata();
-	return $output;
 }
 
 
@@ -71,22 +77,24 @@ function successStories_sc($atts) {
 
     foreach($custom_posts as $post) : setup_postdata($post);
     ?>
-        <h3 class="success-stories__headline"><?php the_title(); ?></h3>
-        
-        <div class="success-stories__content">
-        	<?php the_content(); ?>
-        </div>
+    	<div class="success-story">
+	        <h3 class="success-story__headline"><?php the_title(); ?></h3>
+	        
+	        <div class="success-story__content">
+	        	<?php the_content(); ?>
+	        </div>
+	    </div>
 
-        <div class="success-stories__nav">
+        <div class="success-story__nav">
 		    <?php 
 		    	$next_post = get_next_post();
 		    	$prev_post = get_previous_post(); 
 		    ?>
-	        <a class="success-stories__nav-link success-stories__nav-link--next"<?php if ( is_a( $next_post , 'WP_Post' ) ) : ?> href="<?php echo get_permalink( $next_post->ID ); ?>" title="link to <?php echo get_the_title( $next_post->ID ); ?>"<?php endif; ?>>
+	        <a class="success-story__nav-link success-story__nav-link--next"<?php if ( is_a( $next_post , 'WP_Post' ) ) : ?> href="<?php echo get_permalink( $next_post->ID ); ?>" title="link to <?php echo get_the_title( $next_post->ID ); ?>"<?php endif; ?>>
 				Next post
 			</a>
 			
-			<a class="success-stories__nav-link success-stories__nav-link--prev" <?php if ( is_a( $prev_post , 'WP_Post' ) ) : ?> href="<?php echo get_permalink( $prev_post->ID ); ?>" title="link to <?php echo get_the_title( $prev_post->ID ); ?>" <?php endif; ?>>
+			<a class="success-story__nav-link success-story__nav-link--prev" <?php if ( is_a( $prev_post , 'WP_Post' ) ) : ?> href="<?php echo get_permalink( $prev_post->ID ); ?>" title="link to <?php echo get_the_title( $prev_post->ID ); ?>" <?php endif; ?>>
 					prev post
 			</a>
 		</div>
