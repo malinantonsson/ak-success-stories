bf1d323f50b6a92b9a6b8a46fea4bb347d7f19a1
Malin Antonsson <malin@malinantonsson.com>
2017-04-04 21:21:13 +0100

    start success story markup

--- ak-success-stories.php
+++ ak-success-stories.php
@@ -4,7 +4,7 @@ Plugin Name: Success stories for AK Creative
 Description: Used to dislay success stories
 */
 
-// Our custom post type function
+// Register the new posttype
 function create_posttype() {
 
 	register_post_type( 'success-stories',
@@ -16,23 +16,24 @@ function create_posttype() {
 			),
 			'public' => true,
 			'has_archive' => true,
-			'supports' => array('custom-fields'),
-			'rewrite' => array('slug' => 'success-stories'),
+			'rewrite' => array('slug' => 'success-stories')
 		)
 	);
 }
 // Hooking up our function to theme setup
 add_action( 'init', 'create_posttype' );
 
-// Create the Team shortcode
+
+// Create the archive shortcodes
 add_shortcode("success-story-archive", "successStoriesArchive_sc");
 
+// get all entries & order by date in dsc order
 function successStoriesArchive_sc($atts) {
     global $post;
 
     $args = array(
     	'post_type' => 'success-stories', 
-    	'order'=> 'ASC', 
+    	'order'=> 'DSC', 
     	'orderby' => 'date');
 
     $custom_posts = get_posts($args);
@@ -54,7 +55,7 @@ function successStoriesArchive_sc($atts) {
 }
 
 
-// Create the Team shortcode
+// Create the post shortcode
 add_shortcode("success-story-latest", "successStories_sc");
 
 function successStories_sc($atts) {
@@ -63,28 +64,35 @@ function successStories_sc($atts) {
     $args = array(
     	'post_type' => 'success-stories', 
     	'posts_per_page' => 1, 
-    	'order'=> 'ASC', 
+    	'order'=> 'DSC', 
     	'orderby' => 'date');
 
     $custom_posts = get_posts($args);
 
     foreach($custom_posts as $post) : setup_postdata($post);
-        $output = 'im the output';
-        ?>
-        <h2>Success story post</h2>
-        <a href="<?php the_permalink(); ?>">Link</a><br>
-        title: <?php the_title(); ?> <br>
-        content: <?php the_content(); ?>
+    ?>
+        <h3 class="success-stories__headline"><?php the_title(); ?></h3>
+        
+        <div class="success-stories__content">
+        	<?php the_content(); ?>
+        </div>
 
-        <?php 
-	       $next_post = get_next_post();
-if ( is_a( $next_post , 'WP_Post' ) ) : ?>
-    <a href="<?php echo get_permalink( $next_post->ID ); ?>">Next post: <?php echo get_the_title( $next_post->ID ); ?></a>
-<?php endif; ?>
- 
- 	<?php 
-    	endforeach; wp_reset_postdata();
-		return $output;
+        <div class="success-stories__nav">
+		    <?php 
+		    	$next_post = get_next_post();
+		    	$prev_post = get_previous_post(); 
+		    ?>
+	        <a class="success-stories__nav-link success-stories__nav-link--next"<?php if ( is_a( $next_post , 'WP_Post' ) ) : ?> href="<?php echo get_permalink( $next_post->ID ); ?>" title="link to <?php echo get_the_title( $next_post->ID ); ?>"<?php endif; ?>>
+				Next post
+			</a>
+			
+			<a class="success-stories__nav-link success-stories__nav-link--prev" <?php if ( is_a( $prev_post , 'WP_Post' ) ) : ?> href="<?php echo get_permalink( $prev_post->ID ); ?>" title="link to <?php echo get_the_title( $prev_post->ID ); ?>" <?php endif; ?>>
+					prev post
+			</a>
+		</div>
+    
+    <?php
+    endforeach; wp_reset_postdata();
 	}
 
 ?>
